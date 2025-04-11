e7bbc991fc4278e0adeeadbd31feb38d293c6d49
Lucas Ywamoto <92839243+lucasywamoto@users.noreply.github.com>
2025-04-11 17:00:51 -0400

    set logout and registration

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ myDB(async (client) => {
       title: "Connected to Database",
       message: "Please log in",
       showLogin: true,
+      showRegistration: true,
     });
   });
 
@@ -50,8 +51,49 @@ myDB(async (client) => {
       }
     );
 
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+  app.route("/register").post(
+    (req, res, next) => {
+      myDatabase.findOne({ username: req.body.username }, (err, user) => {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect("/");
+        } else {
+          myDataBase.insertOne(
+            {
+              username: req.body.username,
+              password: req.body.password,
+            },
+            (err, doc) => {
+              if (err) {
+                res.redirect("/");
+              } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0]);
+              }
+            }
+          );
+        }
+      });
+    },
+    passport.authenticate("local", { failureRedirect: "/" }),
+    (req, res, next) => {
+      res.redirect("/profile");
+    }
+  );
+
   app.route("/profile").get(ensureAuthenticated, (req, res) => {
     res.render("profile", { username: req.user.username });
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).type("text").send("Not Found");
   });
 
   passport.use(
