73e86a909b7fe7f57b2e27ff95b702d90a7d6d4a
Lucas Ywamoto <92839243+lucasywamoto@users.noreply.github.com>
2025-04-11 16:01:42 -0400

    set session and passport

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
 
@@ -11,11 +13,23 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+passport.initialize();
+passport.session();
+
 app.set("view engine", "pug");
 app.set("views", "./views/pug");
 
 app.route("/").get((req, res) => {
-  res.render("index");
+  res.render("index", { title: "Hello", message: "Please log in" });
 });
 
 const PORT = process.env.PORT || 3000;
